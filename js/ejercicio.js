febbebfeef92db18ebd4c9c2b770ff2d5b2e74a4
JoSebas007 <joangutierrez2001@gmail.com>
2022-06-20 10:19:54 -0500

    Primer commit

---
+++ js/ejercicio.js
@@ -0,0 +1,54 @@
+const cargarDatos = ()=> {
+    // alert('Cargando datos');
+    fetch('https://dataserverdaw.herokuapp.com/escritores/xml')
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const xml = parser.parseFromString(data, 'application/xml');            
+
+            let escritores = xml.getElementsByTagName('escritor');            
+            
+            for (let escritor of escritores) {
+                let id = escritor.getElementsByTagName('id')[0].textContent;
+                let nombre = escritor.getElementsByTagName('nombre')[0].textContent;
+                // console.log(id, nombre);
+                let plantilla = `<option value="${id}">${nombre}</option>`;
+                document.querySelector('div.input-group > select').innerHTML += plantilla;
+            }
+        })
+        .catch(console.error);
+
+    let select = document.querySelector('div.input-group > select');
+    
+    select.addEventListener('change', function() {
+        fetch('https://dataserverdaw.herokuapp.com/escritores/frases')
+            .then(response => response.json())
+            .then(data => {                
+                let frases = data["frases"];
+                let plantilla, texto, autor;
+
+                document.getElementById('frases').innerHTML = '';
+
+                frases.filter(frase => frase['id_autor'] == select.value)
+                    .forEach(frase => {
+                        texto = frase['texto'];                        
+                        autor = this.options[select.selectedIndex].text;
+                        plantilla = `<div class="col-lg-3">
+                                        <div class="test-inner ">
+                                            <div class="test-author-thumb d-flex">
+                                                <div class="test-author-info">
+                                                    <h4>${autor}</h4>                                            
+                                                </div>
+                                            </div>
+                                            <span>${texto}</span>
+                                            <i class="fa fa-quote-right"></i>
+                                        </div>
+                                    </div>`;
+                        document.querySelector('div#frases').innerHTML += plantilla;
+                    });
+            }).catch(console.error);
+    });
+
+};
+
+window.addEventListener('DOMContentLoaded', (event)=> cargarDatos());
